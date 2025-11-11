d8be73f5536d20ae0d1454211ba56a8c54f745ce
Morshed Alom <morshedalom5675@gmail.com>
2025-11-11 16:42:49 +0600

    contribute api add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 3000;
 
@@ -13,7 +13,6 @@ app.get("/", (req, res) => {
   res.send("Assignment server is running");
 });
 
-
 const uri = `mongodb+srv://${process.env.ASSIGNMENT_USER}:${process.env.ASSIGNMENT_PASS}@cluster0.v3p8d1j.mongodb.net/?appName=Cluster0`;
 
 // assignment10_DB
@@ -24,51 +23,80 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
-    
     await client.connect();
 
-    const db = client.db('assignment10_DB')
-    const issueCollection = db.collection('issue')
+    const db = client.db("assignment10_DB");
+    const issueCollection = db.collection("issue");
+    const contributesCollection = db.collection("contributes");
 
     // all issue get
-    app.get('/issue',async (req, res) => {
-      const result = await issueCollection.find().sort({date:-1}).toArray()
-      res.send(result)
-    })
-
+    app.get("/issue", async (req, res) => {
+      const result = await issueCollection.find().sort({ date: -1 }).toArray();
+      res.send(result);
+    });
 
     // latest issue get
-    app.get('/latest-issue', async (req, res) => {
-      const result = await issueCollection.find().limit(6).sort({date:-1}).toArray()
-      res.send(result)
-    })
-
+    app.get("/latest-issue", async (req, res) => {
+      const result = await issueCollection
+        .find()
+        .limit(6)
+        .sort({ date: -1 })
+        .toArray();
+      res.send(result);
+    });
 
     // single issue get
-    app.get('/issue/:id', async (req, res) => {
-      const id = req.params.id
-      const objectId = new ObjectId(id)
-      const result = await issueCollection.findOne({_id:objectId})
+    app.get("/issue/:id", async (req, res) => {
+      const id = req.params.id;
+      const objectId = new ObjectId(id);
+      const result = await issueCollection.findOne({ _id: objectId });
+      res.send(result);
+    });
+
+    // single issue post
+    app.post("/issue", async (req, res) => {
+      const data = req.body;
+      const result = await issueCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // contribute collection api
+
+    // single contribute post
+    app.post("/contributes", async (req, res) => {
+      const data = req.body;
+      const result = await contributesCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // issue contribution table
+    app.get('/issue/contributes/:issueId', async(req, res) => {
+      const issueId = req.params.issueId
+      const query = { issue: issueId }
+      const cursor =  contributesCollection.find(query).sort({date:-1}) 
+      const result = await cursor.toArray()
       res.send(result)
     })
 
+    // logged user issues contribution
+    app.get("/contributes", async (req, res) => {
+      const query = {};
+      if (query.email) {
+        query.contributor_email = email;
+      }
+      const result = await contributesCollection.find(query).toArray();
+      res.send(result);
+    });
 
-
-    // single data post 
-    app.post('/issue', async (req, res) => {
-      const data = req.body
-      const result = await issueCollection.insertOne(data)
-      res.send(result)
-    })
-    
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
     // await client.close();
   }
 }
